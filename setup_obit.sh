4a1c58cee82ac7129cf6eb34c551721cdb915bb7
Simon Perkins <simon.perkins@gmail.com>
2017-06-30 16:57:29 +0200

    Improve ENV variables a bit more

--- setup_obit.sh
+++ setup_obit.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-OBIT_BASE_PATH=/src/Obit
+export OBIT_BASE_PATH=/src/Obit
+export OBIT=$OBIT_BASE_PATH/trunk/ObitSystem/Obit
+
 
 cd $OBIT_BASE_PATH
 patch -p0 -N --dry-run --silent < /obit.patch 2>/dev/null
@@ -15,6 +17,10 @@ cd $OBIT_BASE_PATH/trunk/ObitSystem/Obit
 ./configure --prefix=/usr --without-plplot --without-wvr
 make -j 8
 
+# So we can import libObit.so
 export LD_LIBRARY_PATH=$OBIT_BASE_PATH/trunk/ObitSystem/Obit/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=$OBIT_BASE_PATH/trunk/ObitSystem/Obit/python:$PYTHONPATH
+# So we can import Obit.so and OTObit.py
+export PYTHONPATH=$OBIT/python:$PYTHONPATH
+# So OTObit.py can import AIPS.py
+export PYTHONPATH=$OBIT_BASE_PATH/trunk/ObitSystem/ObitTalk/python:$PYTHONPATH
 
