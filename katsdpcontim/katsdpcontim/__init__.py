fd2b2c8bfb8a8a014e6b14ecc3df34003654775f
Simon Perkins <simon.perkins@gmail.com>
2017-08-24 10:51:44 +0200

    Add a package logger

--- katsdpcontim/katsdpcontim/__init__.py
+++ katsdpcontim/katsdpcontim/__init__.py
@@ -1,3 +1,7 @@
+from logsetup import get_logger
+
+log = get_logger()
+
 from katdal_adapter import KatdalAdapter
 from uv_facade import UVFacade
 
@@ -5,3 +9,4 @@ from obit_context import (obit_context,
                           obit_err,
                           obit_sys,
                           handle_obit_err)
+
