5fb7a18121fdba00cda73fda783cdc3c23b1a2c3
Simon Perkins <simon.perkins@gmail.com>
2017-08-30 17:14:34 +0200

    task_factory for generically creating Obit tasks

--- katsdpcontim/katsdpcontim/util/__init__.py
+++ katsdpcontim/katsdpcontim/util/__init__.py
@@ -1,6 +1,8 @@
 import ast
 import logging
 
+from katsdpcontim.configuration import get_config
+
 log = logging.getLogger('katsdpcontim')
 
 def parse_katdal_select(select_str):
@@ -42,3 +44,68 @@ def parse_katdal_select(select_str):
         log.exception("Exception parsing katdal selection string "
                     "'{}'".format(select_str))
         raise e
+
+def task_factory(name, aips_cfg_file=None, **kwargs):
+    """
+    Creates Obit tasks, applying any configuration options
+    supplied in an AIPS configuration file and keyword arguments.
+
+    The following creates an `MFImage` task, loading configuration
+    options from `mfimage.in` and setting the name, class and
+    sequence number of the input AIPS file on disk 1.
+
+    .. code-block:: python
+
+        mfimage = task_factory("MFImage", "mfimage.in",
+                            DataType="AIPS", inName="DEEP2",
+                            inClass="raw", inSeq=1, inDisk=1)
+        mfimage.go()
+
+    Invalid configuration options will generate warnings,
+    but will not be applied to the task.
+
+    Parameters
+    ----------
+    name: string
+        Obit task name. "MFImage" for example.
+    aips_cfg_file (optional): string
+        AIPS config file
+    **kwargs (optional): :obj:
+        Any task options that should be applied
+        to the returned task object.
+
+    Returns
+    -------
+    :class:`ObitTask.ObitTask`
+        Obit task object
+    """
+
+    import ObitTask
+    from katsdpcontim.aips_parser import (obit_config_from_aips,
+                                        apply_cfg_to_task)
+
+    # Load any supplied file configuration first,
+    # then apply kwargs on top of this
+    if aips_cfg_file is not None:
+        file_kwargs = obit_config_from_aips(aips_cfg_file)
+        file_kwargs.update(kwargs)
+        kwargs = file_kwargs
+
+    # Override user number and directories
+    # from global configuration
+    global_cfg = get_config()
+    kwargs['user'] = global_cfg.aips.userno
+    kwargs['AIPSdirs'] = [dir for url, dir in global_cfg.obit.aipsdirs]
+    kwargs['FITSdirs'] = [dir for url, dir in global_cfg.obit.fitsdirs]
+
+    # Obit ignores these options
+    for k in ('nFITS', 'nAIPS', 'AIPSuser'):
+        kwargs.pop(k, None)
+
+    # Create the Obit Task
+    task = ObitTask.ObitTask(name)
+
+    # Apply configuration options to the task
+    apply_cfg_to_task(task, kwargs)
+
+    return task
