5a04c0bc0e739081298c39a43fdf327346b4a30b
Simon Perkins <simon.perkins@gmail.com>
2017-08-30 17:40:04 +0200

    AIPS configuration handling cleanup

--- katsdpcontim/katsdpcontim/__init__.py
+++ katsdpcontim/katsdpcontim/__init__.py
@@ -7,6 +7,7 @@ from obit_context import (obit_context,
                           obit_sys,
                           handle_obit_err)
 
+from configuration import get_config
 from katdal_adapter import KatdalAdapter
 from aips_table import AIPSTable
 from uv_facade import (UVFacade, open_uv,
--- katsdpcontim/katsdpcontim/aips_parser.py
+++ katsdpcontim/katsdpcontim/aips_parser.py
@@ -6,7 +6,9 @@ import six
 import InfoList
 import ParserUtil
 
+import katsdpcontim
 from katsdpcontim import obit_err, handle_obit_err
+from katsdpcontim.obit_types import OBIT_TYPE_ENUM, OBIT_TYPE
 
 log = logging.getLogger('katsdpcontim')
 
@@ -50,7 +52,7 @@ def parse_aips_config(aips_cfg_file):
 
     return InfoList.PGetDict(info_list)
 
-def process_aips_config(aips_cfg_file):
+def obit_config_from_aips(aips_cfg_file):
     """
     Extract key-values from AIPS configuration file
     into a { option: value } dictionary.
@@ -79,98 +81,31 @@ def process_aips_config(aips_cfg_file):
         # Split into type, dimensions and value
         type_, dims, value = option
 
-        # Booleans integers must be cast to python bools
-        if type_ == 15:
-            value = [bool(v) for v in value]
+        assert isinstance(value, list)
+
+        enum = OBIT_TYPE_ENUM[type_]
+
+        is_str = enum.name == "string"
+
+        # Coerce values into their python equivalents
+        if is_str:
+            value = [enum.coerce(v).ljust(dims[0], ' ') for v in value]
+        else:
+            value = [enum.coerce(v) for v in value]
 
         # Check second dimension to test singletons
         # if we're handling strings else the first dim
-        check_dim = 1 if type_ == 14 else 0
+        check_dim = 1 if is_str else 0
 
-        # Return element zero from singleton lists
+        # Return first element from singleton lists
         if dims[check_dim] == 1 and len(value) == 1:
             return value[0]
 
         return value
 
     return { k: _massage(o) for k, o
-            in parse_aips_config(aips_cfg_file).iteritems()}
+            in parse_aips_config(aips_cfg_file).iteritems() }
 
-
-
-from AIPS import AIPS
-from FITS import FITS
-
-def aips_disk_config(infile, fitsdir, aipsdir):
-    """
-    Returns an AIPS disk configuration suitable for
-    use with Obit
-    """
-
-    import os
-
-    if infile is None:
-        infile = ""
-        log.warn("No 'infile' was provided, setting to '%s'" % infile)
-
-    if fitsdir is None:
-        fitsdir = FITS.disks[aipsdisk].dirname
-        log.warn("No 'fitsdir' was provided, setting to '%s'" % fitsdir)
-
-    if aipsdir is None:
-        aipsdir = AIPS.disks[fitsdisk].dirname
-        log.warn("No 'aipsdir' was provided, setting to '%s'" % aipsdir)
-
-    # Override AIPS disk configuration options
-    cfg = OrderedDict([
-        ("DataType", "FITS"),
-        ("FITSdirs", [fitsdir]),
-        ("AIPSdirs", [aipsdir]),
-        ("inFile", infile),
-        ("inDisk", 0),
-        ("inSeq", 0),
-        ("outFile", ".out.fits"),
-        ("out2File", ".out.fits")])
-
-    # Set output disk options to input disk options
-    cfg.update([
-        ("outDType", cfg["DataType"]),
-        ("outDisk", cfg["inDisk"]),
-        ("outSeq", cfg["inSeq"]),
-        ("out2Disk", cfg["inDisk"]),
-        ("out2Seq", cfg["inSeq"]),
-    ])
-
-    return cfg
-
-def aips_user():
-    """ Get the AIPS user """
-    import OSystem
-
-    try:
-        return OSystem.PGetAIPSuser()
-    except Exception as e:
-        log.exception("Exception getting AIPS User. "
-                          "Returning 105 instead")
-        return 105
-
-def aips_cfg(aips_cfg_file, infile=None, fitsdir=None, aipsdir=None):
-    """ Construct a usable AIPS configuration """
-
-    # Parse the configuration file
-    cfg = process_aips_config(aips_cfg_file)
-
-    # Set the user file
-    cfg['userno'] = aips_user()
-
-    # Set up some reasonable AIPS disk config
-    cfg.update(aips_disk_config(infile, fitsdir, aipsdir))
-
-    # These don't work with OBIT for some reason
-    for k in ('nFITS','nAIPS', 'AIPSuser'):
-        cfg.pop(k, None)
-
-    return cfg
 
 def apply_cfg_to_task(task, cfg):
     """
