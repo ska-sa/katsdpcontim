3a224d5f50e0043019071ab03cacf1d6dfafad58
Tom Mauch <tmauch@ska.ac.za>
2021-03-09 14:20:15 +0000

    Use katversion.

--- katacomb/setup.py
+++ katacomb/setup.py
@@ -1,30 +1,12 @@
-import codecs
 from glob import glob
-import os
 from os.path import join as pjoin
-import re
 
 from setuptools import setup, find_packages
 
 PKG = 'katacomb'
-
-def find_version():
-    """ Extract version from version.py """
-    version_file = pjoin(PKG, 'version.py')
-
-    with codecs.open(version_file, 'r', encoding='utf-8') as f:
-        VERSION_RE = r"^__version__ = ['\"]([^'\"]*)['\"]"
-        version_match = re.search(VERSION_RE, f.read(), re.M)
-
-        if version_match:
-            return version_match.group(1)
-
-        raise RuntimeError("Unable to find version string.")
-
 DESCRIPTION = "MeerKAT SDP Continuum Pipeline"
 
 setup(name=PKG,
-    version=find_version(),
     description=DESCRIPTION,
     long_description=DESCRIPTION,
     url='https://github.com/ska-sa/katsdpcontim',
@@ -39,6 +21,7 @@ setup(name=PKG,
     ],
     author='MeerKAT SDP Team',
     author_email='sdpdev+katsdpcontim@ska.ac.za',
+    python_requires=">=3.6",
     install_requires=['astropy',
                       'attrs',
                       'cerberus>=1.1',
@@ -56,5 +39,6 @@ setup(name=PKG,
                       'scipy'],
     scripts=glob(pjoin('scripts', '*.py')),
     packages=find_packages(),
-    package_data={PKG: [pjoin('conf', '*.in')]}
+    package_data={PKG: [pjoin('conf', '*.in')]},
+    use_katversion=True
 )
